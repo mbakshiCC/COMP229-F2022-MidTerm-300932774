a115ad03de5261e683a41237d212d950ed4d5f8e
mbakshiCC <mbakshi1@my.centennialcollege.ca>
2022-10-30 21:46:42 -0400

    initialcommit

--- server/routes/books.js
+++ server/routes/books.js
@@ -17,28 +17,29 @@ router.get('/', (req, res, next) => {
                 title: 'Books',
                 books: books
             });
-            console.log(books);
+            console.log(books); //MB: Prints the value of books   
         }
     });
 
 });
 //  GET the Book Details page in order to add a new Book
+// The 
 router.get('/add', (req, res, next) => {
     let books = require('../models/books');
-    res.render('books/details', { title: 'Add a new  Book', books })
+    res.render('books/details', { title: 'Add a new  Book', books }) // MB takes the user input
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-    console.log(req);
+    console.log(req); // takes the format from models/books.js
     let newBook = book({
         "Title": req.body.title,
         "Author": req.body.author,
         "Price": req.body.price,
         "Genre": req.body.genre
     });
-    book.create(newBook, (err, Book) => {
+    book.create(newBook, (err, Book) => { // MB will create the records field
         if (err) {
             console.log(err);
             res.end(err);
@@ -52,7 +53,7 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    let id = req.params.id;
+    let id = req.params.id; // MB stores the user input here
     book.findById(id, (err, bookToEdit) => {
         if (err) {
             console.log(err);
