c1b2385a51634a0b31e63b2e782e00c778be9017
mbakshiCC <mbakshi1@my.centennialcollege.ca>
2022-10-30 21:57:03 -0400

    dbs update-01

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     //local MongoDB deployment ->
-    "URI": const uri = "mongodb+srv://mbakshi1:<password>@cluster0.zxxd6qs.mongodb.net/?retryWrites=true&w=majority";
+    "URI": "mongodb+srv://mbakshi1:<password>@cluster0.zxxd6qs.mongodb.net/?retryWrites=true&w=majority";
 };
